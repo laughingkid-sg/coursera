95238f0a3e99218fb6e922bdc0f6c7f9305a0376
laughingkid-sg <zhengteck.goh@comp.nus.edu.sg>
2021-06-17 01:02:33 +0800

    Basic Authentication

--- Server-side Development - HKUST/express-generator/conFusionServer/app.js
+++ Server-side Development - HKUST/express-generator/conFusionServer/app.js
@@ -32,6 +32,30 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+function auth (req, res, next) {
+  console.log(req.headers);
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      next(new Error('You are not authenticated!'));
+      return;
+  }
+  const auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  const user = auth[0];
+  const pass = auth[1];
+  if (user == 'admin' && pass == 'password') {
+      next(); // authorized
+  } else {
+      res.setHeader('WWW-Authenticate', 'Basic');      
+      err.status = 401;
+      next(new Error('You are not authenticated!'));
+  }
+}
+
+app.use(auth);
+  
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
