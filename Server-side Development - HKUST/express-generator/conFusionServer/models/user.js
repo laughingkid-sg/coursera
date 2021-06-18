ce0b77ff19164703facddaf984086ce10d326260
laughingkid-sg <zhengteck.goh@comp.nus.edu.sg>
2021-06-18 23:28:41 +0800

    Mongoose Population

--- Server-side Development - HKUST/express-generator/conFusionServer/models/user.js
+++ Server-side Development - HKUST/express-generator/conFusionServer/models/user.js
@@ -4,7 +4,15 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
 
 const User = new Schema({
-    admin:   {
+    firstname: {
+        type: String,
+          default: ''
+      },
+      lastname: {
+        type: String,
+          default: ''
+      },
+        admin:   {
         type: Boolean,
         default: false
     }
