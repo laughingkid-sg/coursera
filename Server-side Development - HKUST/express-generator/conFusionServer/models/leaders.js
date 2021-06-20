a629319a042bab13a64266d912c5680511acac54
laughingkid-sg <zhengteck.goh@comp.nus.edu.sg>
2021-06-20 16:36:44 +0800

    Assignment 2 Submission

---
+++ Server-side Development - HKUST/express-generator/conFusionServer/models/leaders.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const leaderSchema  = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    designation: {
+        type: String,
+        required: true,
+    },
+    abbr: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    featured: {
+      type: Boolean,
+      required: false
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Leader', leaderSchema);
--- Server-side Development - HKUST/express-generator/conFusionServer/routes/leaderRouter.js
+++ Server-side Development - HKUST/express-generator/conFusionServer/routes/leaderRouter.js
@@ -1,52 +1,86 @@
 const express = require('express');
 const authenticate = require('../authenticate');
+const Leaders = require('../models/leaders');
 
 const leaderRouter = express.Router();
 
 leaderRouter.use(express.json());
 
 leaderRouter.route('/')
-    .all((req, res, next) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        next();
+    .get((req,res,next) => {
+        Leaders.find({})
+        .then((leaders) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');    
+            res.json(leaders);
+        }, (err) => next(err))
+        .catch((err) => next(err));
     })
 
-    .get((req, res, next) => {
-        res.end('Will send all the leaders to you!');
+    .post((req, res, next) => {
+        Leaders.create(req.body)
+        .then((leader) => {
+            console.log('Leader Created ', leader);
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');    
+            res.json(leader);
+        }, (err) => next(err))
+        .catch((err) => next(err));
     })
 
-    .post(authenticate.verifyUser, (req, res, next) => {
-        res.end('Will add the leader: ' + req.body.name + ' with details: ' + req.body.description);
-    })
-
-    .put(authenticate.verifyUser, (req, res, next) => {
+    .put((req, res, next) => {
         res.statusCode = 403;
-        res.end('PUT operation not supported on /dishes');
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('PUT operation not supported on /leaders');
     })
 
-    .delete(authenticate.verifyUser, (req, res, next) => {
-        res.end('Deleting all leaders');
+    .delete((req, res, next) => {
+        Leaders.remove({})
+        .then((resp) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');    
+            res.json(resp);
+        }, (err) => next(err))
+        .catch((err) => next(err));
     });
 
 leaderRouter.route('/:leaderId')
-    .all((req, res, next) => {
+    .get((req,res,next) => {
+        Leaders.findById(req.params.leaderId)
+        .then((leader) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');    
+            res.json(leader);
+        }, (err) => next(err))
+        .catch((err) => next(err));
+    })
+
+    .post((req, res, next) => {
+        res.statusCode = 403;
         res.setHeader('Content-Type', 'text/plain');
-        next();
+        res.end('POST operation not supported on /leaders/'+ req.params.leaderId);
     })
 
-    .get((req, res, next) => {
-        res.end('Will send details of the leader: ' + req.params.leaderId + ' to you!');
+    .put((req, res, next) => {
+        Leaders.findByIdAndUpdate(req.params.leaderId, {
+            $set: req.body
+        }, { new: true })
+        .then((leader) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');            
+            res.json(leader);
+        }, (err) => next(err))
+        .catch((err) => next(err));
     })
-
-    .put(authenticate.verifyUser, (req, res, next) => {
-        res.write('Updating the leader: ' + req.params.leaderId + '\n');
-        res.end('Will update the leader: ' + req.body.name +
-            ' with details: ' + req.body.description);
-    })
-
-    .delete(authenticate.verifyUser, (req, res, next) => {
-        res.end('Deleting leader: ' + req.params.leaderId);
+    
+    .delete((req, res, next) => {
+        Leaders.findByIdAndRemove(req.params.leaderId)
+        .then((resp) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');    
+            res.json(resp);
+        }, (err) => next(err))
+        .catch((err) => next(err));
     });
 
 module.exports = leaderRouter;
