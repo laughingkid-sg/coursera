71d86b48722463a24f4e79a1ad185ad49770682a
laughingkid-sg <zhengteck.goh@comp.nus.edu.sg>
2021-06-23 12:37:47 +0800

    Passport Facebook

--- Server-side Development - HKUST/express-generator/conFusionServer/authenticate.js
+++ Server-side Development - HKUST/express-generator/conFusionServer/authenticate.js
@@ -3,9 +3,11 @@ const LocalStrategy = require('passport-local').Strategy;
 const User = require('./models/user');
 require('dotenv').config()
 
-var JwtStrategy = require('passport-jwt').Strategy;
-var ExtractJwt = require('passport-jwt').ExtractJwt;
-var jwt = require('jsonwebtoken');
+const FacebookTokenStrategy = require('passport-facebook-token');
+
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const jwt = require('jsonwebtoken');
 
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
@@ -47,3 +49,30 @@ exports.verifyAdmin = (req, res, next) => {
         return next(err);
     }
 }
+
+exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+    clientID: process.env.FBID,
+    clientSecret: process.env.FBSECRET
+}, (accessToken, refreshToken, profile, done) => {
+    User.findOne({facebookId: profile.id}, (err, user) => {
+        if (err) {
+            return done(err, false);
+        }
+        if (!err && user !== null) {
+            return done(null, user);
+        }
+        else {
+            user = new User({ username: profile.displayName });
+            user.facebookId = profile.id;
+            user.firstname = profile.name.givenName;
+            user.lastname = profile.name.familyName;
+            user.save((err, user) => {
+                if (err)
+                    return done(err, false);
+                else
+                    return done(null, user);
+            })
+        }
+    });
+}
+));
